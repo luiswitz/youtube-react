4b678b7aa6306e00050d537bcbd0dcf2735bff4a
Luis Henrique Witz <luiswitz@gmail.com>
2017-08-04 21:21:06 -0300

    Project setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {API_KEY} from './keys';
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
